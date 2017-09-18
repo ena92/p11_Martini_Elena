803bd70b143ce3dd5dfd271f849d7f0482358e51
Elena Martini <elepepsteka@gmail.com>
2017-09-18 12:39:10 +0200

    Test messi in cartella tests

--- test_funzionali/TestSuiteFunzionali.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runners.Suite;
-import org.junit.runner.RunWith;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({PreventivoTestFunzionali.class, AutenticazioneTestFunzionali.class, CinemaTestFunzionali.class, SalaTestFunzionali.class, FilmTestFunzionali.class, RegistrazioneTestFunzionali.class})
-
-public class TestSuiteFunzionali {
-
-}
